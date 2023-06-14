6db2b1e2aa62e425f6c1128212a092a1ed6c17e8
ChaseBENNC9 <104808214+ChaseBENNC9@users.noreply.github.com>
2023-06-14 22:48:54 +1200

    Added comments to methods of MoveData script

--- Assets/Scripts/MoveData.cs
+++ Assets/Scripts/MoveData.cs
@@ -6,6 +6,6 @@ using UnityEngine;
 //Gives a score and index for each move the minimax algorithm can make.
 public class MoveData
 {
-    public int score;
-    public int index;
+    public int score; //The Value of the Move
+    public int index; //The Index of the selected tile
 }
